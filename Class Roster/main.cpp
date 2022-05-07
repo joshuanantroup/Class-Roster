ba80b7cd2156affbdd15bdf37c69f924ea63bd6e
Josh Nantroup <jnantroup@gmail.com>
2022-05-07 12:26:13 -0500

    Initial Commit

---
+++ Class Roster/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Class Roster
+//
+//  Created by Josh Nantroup on 5/7/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
